eba5e9c3cf036b70428e7db28e187fdeaea4322a
sapphaos <mewapopo@gmail.com>
2019-03-20 16:04:18 +0100

    toggle validation state

--- src/app/operations/details/details.page.ts
+++ src/app/operations/details/details.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { OperationDbService } from '../operations-db.service';
+import { ActivatedRoute } from '@angular/router';
+import { Operation } from 'src/app/model/operation';
+import { IonCheckbox } from '@ionic/angular';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
+  @ViewChild('checkbox') checkbox: IonCheckbox;
 
-  constructor() { }
+  operation: Operation;
+  checkboxtext: String;
+
+  constructor(private operationDbService: OperationDbService, private route: ActivatedRoute) {
+    this.operation = new Operation();
+  }
 
   ngOnInit() {
+    const operationId: String = this.route.snapshot.paramMap.get('id');
+    if (operationId) {
+      this.getOperationInformations(operationId);
+    }
+    this.loadCheckbox();
+  }
+
+  async getOperationInformations(areaId: String) {
+    this.operation.id = +areaId;
+    this.operation = await this.operationDbService.getOperationById(this.operation.id);
+  }
+
+  private loadCheckbox() {
+    if (this.operation.done === true) {
+      this.checkbox.checked = true;
+      this.checkboxtext = ' Opération effectuée ';
+    } else {
+      this.checkbox.checked = false;
+      this.checkboxtext = ' Opération non effectuée ';
+    }
+  }
+
+  changeCheckbox() {
+    if (this.checkbox.checked === true) {
+      this.operation.done = true;
+      this.checkboxtext = ' Opération effectuée ';
+    } else {
+      this.operation.done = false;
+      this.checkboxtext = ' Opération non effectuée ';
+    }
   }
 
 }
