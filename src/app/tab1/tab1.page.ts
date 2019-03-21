ef25a3a5a70e73f10648d6ddb4067951211e2ab3
Maxime DURAND <maxime.durand1@univ-tlse3.fr>
2019-03-21 18:40:10 +0100

    Add refresh planning component 🎆

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
-
+import { PlanningComponent } from '../operations/planning/planning.component';
+import { Component, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  @ViewChild('appPlanning')
+  private test: PlanningComponent ;
+
+  ionViewDidEnter() {
+    this.test.ngOnInit();
+  }
+}
