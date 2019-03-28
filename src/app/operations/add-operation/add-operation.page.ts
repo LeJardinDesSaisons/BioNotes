3bddaaa358497927e226b4b02f009a240fb36e0d
YannCami <caminadeyann@gmail.com>
2019-03-28 16:16:17 +0100

    Set default date to today's date in operation form

--- src/app/operations/add-operation/add-operation.page.ts
+++ src/app/operations/add-operation/add-operation.page.ts
@@ -1,3 +1,4 @@
+import { Operation } from '../../model/operation';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddOperationPage implements OnInit {
 
-  constructor() { }
+  operation: Operation;
+
+  constructor() {
+    this.operation = new Operation();
+    this.operation.date = new Date().toISOString();
+  }
 
   ngOnInit() {
   }
--- src/app/operations/form-operation/form-operation.component.ts
+++ src/app/operations/form-operation/form-operation.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Operation } from 'src/app/model/operation';
 
 @Component({
   selector: 'form-operation',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-operation.component.scss']
 })
 export class FormOperationComponent implements OnInit {
+
+  @Input() operation: Operation;
 
   constructor() { }
 
